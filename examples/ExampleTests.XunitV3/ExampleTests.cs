f6526fbd9a8f6b2d704534644bf574a559a420a5
metacoder_FEFF <metacoder.feff@gmail.com>
2026-03-15 12:08:50 +0000

    add ExampleTests.XunitV3

---
+++ examples/ExampleTests.XunitV3/ExampleTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using AwesomeAssertions;
+using FEFF.TestFixtures;
+using Xunit.v3;
+
+[assembly: FEFF.TestFixtures.Xunit.TestFixturesExtension]
+
+namespace ExampleTests.XunitV3;
+
+public class SystemUnderTest
+{
+    public static void Write(string filePath) =>
+        File.WriteAllText(filePath, "some-data", Encoding.UTF8);
+}
+
+public class ExampleTests
+{
+    protected TmpDirectoryFixture TmpDir { get; } = TestContext.Current.GetFeffFixture<TmpDirectoryFixture>();
+
+    [Fact]
+    public void File__should_be_created()
+    {
+        // Arrange
+        var filePath = TmpDir.Path + "/file.tmp";
+        File.Exists(filePath).Should().BeFalse();
+
+        // Act
+        SystemUnderTest.Write(filePath);
+
+        // Assert
+        File.Exists(filePath)
+            .Should().BeTrue();
+
+        File.ReadAllText(filePath, Encoding.UTF8)
+            .Should().Be("some-data");
+    }
+}
