c301f11e2a1bd302ba75e0274b085de80a91bd55
metacoder_FEFF <metacoder.feff@gmail.com>
2026-03-17 17:07:43 +0000

    clear utils

--- src/FEFF.TestFixtures/Utils/CollectionsExtentions.cs
+++ src/FEFF.TestFixtures/Utils/CollectionsExtentions.cs
@@ -1,13 +1,9 @@
-using System.Diagnostics.CodeAnalysis;
-
 //TODO: link nuget
 
-// namespace System.Collections.Generic;
-namespace System; // Array
+namespace System.Collections.Generic;
 
 internal static class CollectionsExtentions
 {
-//TODO: single method?
     public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> sequence)
     where T : struct
     {
@@ -19,21 +15,7 @@ internal static class CollectionsExtentions
             yield return item.Value;
         }
     }
-
-//TODO: test
-    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> sequence)
-    where T : notnull
-    {
-        // return enumerable.Where(e => e != null).Select(e => e!);
-        foreach (var item in sequence)
-        {
-            if (item == null)
-                continue;
-            yield return item;
-        }
-    }
     
-//TODO: test
     // returns 'null' only when not found. Only allowed when 'TVal : notnull'.
     public static TVal? TryGetOrNull<TKey, TVal>(this IDictionary<TKey, TVal>src, TKey key)
     where TVal : notnull
@@ -42,48 +24,5 @@ internal static class CollectionsExtentions
         if (b == false)
             return default;
         return value;
-    }
-
-//TODO: test
-    // returns 'null' whether not found or found null
-    public static TVal? TryGetBothNull<TKey, TVal>(this IDictionary<TKey, TVal?>src, TKey key)
-    where TVal : notnull
-    {
-        var b = src.TryGetValue(key, out var value);
-        if (b == false)
-            return default;
-        return value;
-    }
-
-    public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this IReadOnlyCollection<T>? src)
-    {
-        if (src == null)
-            return true;
-
-        return src.Count <= 0;
-    }
-
-    public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this T[]? src)
-    {
-        if (src == null)
-            return true;
-
-        return src.Length <= 0;
-    }
-
-    public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this List<T>? src)
-    {
-        if (src == null)
-            return true;
-
-        return src.Count <= 0;
-    }
-
-    public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this ICollection<T>? src)
-    {
-        if (src == null)
-            return true;
-
-        return src.Count <= 0;
     }
 }
