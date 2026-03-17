f4f5a8e0223dc5067a46ed36872f0e6bc5c7c7bd
metacoder_FEFF <metacoder.feff@gmail.com>
2026-03-17 10:30:59 +0000

    better

--- src/FEFF.TestFixtures/Core/FixtureCollector.cs
+++ src/FEFF.TestFixtures/Core/FixtureCollector.cs
@@ -6,15 +6,20 @@ namespace FEFF.TestFixtures.Core;
 
 internal static class FixtureCollector
 {
+    /// <remarks>
+    /// Heavy operation. Better to memoize the result.
+    /// </remarks>
     internal static ServiceCollection CreateServiceCollection()
     {
         var services = new ServiceCollection();
 
         AddBaseServices(services);
 
-        var types = FindFixtureTypes<FixtureAttribute>();
-        foreach (var t in types)
-            RegisterFixtureType(services, t);
+        foreach(var t in GetAllLoadedTypes())
+        {
+            services.TryAddFixture(t);
+            services.TryRegisterExtended(t);
+        }
         
         return services;
     }
@@ -30,33 +35,26 @@ internal static class FixtureCollector
         services.AddSingleton<IConfiguration>(configuration);
     }
 
-    private static IEnumerable<Type> FindFixtureTypes<TAttribute>()
-    {
-        var atr = typeof(TAttribute);
-        
-        return GetAssemblies()
+//TODO: optimize?  
+    private static IEnumerable<Type> GetAllLoadedTypes() =>
+        AppDomain.CurrentDomain
+            .GetAssemblies()
             .SelectMany(a => a.GetTypes())
-            .Where(t => t.IsDefined(atr, false));
-    }
+            ;
 
-    private static IEnumerable<Assembly> GetAssemblies()
+    private static void TryAddFixture(this ServiceCollection services, Type t)
     {
-//TODO: optimize?        
-        // var assembly = Assembly.GetExecutingAssembly();
-        // return [assembly];
-        
-        return AppDomain.CurrentDomain.GetAssemblies();
-    }
-
-    private static void RegisterFixtureType(ServiceCollection services, Type t)
-    {
-//TODO: external scan
-        TryInvokeRegisterExtended(t, services);
-
-        services.AddScoped(t);
         var attribute = t.GetCustomAttribute<FixtureAttribute>();
-        if (attribute?.RegisterWithType is null)
+        if(attribute == null)
             return;
+
+        // register primary type
+        services.AddScoped(t);
+
+        // register by supertype
+        if (attribute.RegisterWithType is null)
+            return;
+
 //TODO: analizer
         if(attribute.RegisterWithType.IsAssignableFrom(t) == false)
             throw new InvalidOperationException($"The implementation type'{t}' should be a subtype or implement {nameof(FixtureAttribute.RegisterWithType)} '{attribute.RegisterWithType}'.");
@@ -64,14 +62,12 @@ internal static class FixtureCollector
         services.AddScoped(attribute.RegisterWithType, sp => sp.GetRequiredService(t));
     }
 
-    private static void TryInvokeRegisterExtended(Type t, ServiceCollection services)
+    private static void TryRegisterExtended(this ServiceCollection services, Type t)
     {
         if(t.GetInterfaces().Contains(typeof(IFixureRegistrator)) == false)
             return;
 
-        var mi = typeof(FixtureCollector).GetMethod(nameof(RegisterExtended));
-        var tt = typeof(FixtureCollector).GetMethods();
-
+//TODO: memoize?
         var method = ThrowHelper.Guard.NotNull(
             typeof(FixtureCollector).GetMethod(nameof(RegisterExtended), BindingFlags.NonPublic | BindingFlags.Static)
         );
@@ -82,6 +78,8 @@ internal static class FixtureCollector
             ;
     }
 
+    // Since an interface implementation may have a different method name, 
+    // it's safer to call it through static linking rather than directly via reflection.
     private static void RegisterExtended<T>(ServiceCollection services)
     where T: IFixureRegistrator
     {
